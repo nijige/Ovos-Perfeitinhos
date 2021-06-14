3824ecaede6af795b3c4be8f9e72d2c9e2971d6c
Geane Ferreira Pereira <geane3096@gmail.com>
2021-06-14 12:57:59 -0400

    gestão de estado pronta

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:ovinhos_perfeitos/blocs/app.bloc.dart';
 import 'package:ovinhos_perfeitos/pages/splash.page.dart';
 import 'package:ovinhos_perfeitos/themes/theme.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppBloc>.value(
+          value: AppBloc(),
+        )
+      ],
+      child: Main(),
+    );
+  }
+}
+
+class Main extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
